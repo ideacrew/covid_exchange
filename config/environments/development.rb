db7030b9ae39d07348f72fc28795514a96e711e8
Bill Transue <transue@gmail.com>
2020-04-24 14:24:50 -0400

    Configure dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,10 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.active_record.belongs_to_required_by_default = false
+
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
